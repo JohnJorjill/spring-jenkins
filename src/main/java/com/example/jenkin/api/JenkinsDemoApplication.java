1f5c99b4be61a4ff1b4329b5660d6032c1a89dfa
Bat-Ulzii <obat3003@al003110407.7-11.com>
2022-02-09 01:04:12 -0600

    first commit

---
+++ src/main/java/com/example/jenkin/api/JenkinsDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void intt() {
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
